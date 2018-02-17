a5b8c4d37c21c4388c4cf030b26308b7d5a60d30
evgen <andev.amconsoft1@gmail.com>
2018-02-18 01:36:26 +0200

    ViewModel structure(base implementation)

--- app/src/main/java/com/railsreactor/moviedbapp/presentation/main/MainActivity.java
+++ app/src/main/java/com/railsreactor/moviedbapp/presentation/main/MainActivity.java
@@ -1,19 +1,33 @@
 package com.railsreactor.moviedbapp.presentation.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
 import com.railsreactor.moviedbapp.R;
+import com.railsreactor.moviedbapp.databinding.ActivityMainBinding;
+import com.railsreactor.moviedbapp.presentation.base.MvvmLoadingActivity;
+
+import javax.inject.Inject;
+
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.support.HasSupportFragmentInjector;
 
 /**
  * @author Evgeny Kubay on 2/17/18.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends MvvmLoadingActivity<ActivityMainBinding, MainActivityViewModel> implements HasSupportFragmentInjector {
+
+    @Inject DispatchingAndroidInjector<Fragment> dispatchingAndroidInjector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setAndBindContentView(this, savedInstanceState, R.layout.activity_main);
+    }
+
+    @Override
+    public DispatchingAndroidInjector<Fragment> supportFragmentInjector() {
+        return dispatchingAndroidInjector;
     }
 }
